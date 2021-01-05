ab04a105b9ab926b5763d7695a12295a8dccbcf5
Leo.li <liyang.fx@163.com>
2021-01-05 14:46:19 +0800

    add new file about point and array.

--- Pointer/pointer_array1.c
+++ Pointer/pointer_array1.c
@@ -3,10 +3,15 @@
 int main()
 {
 		int a[5] = {1,2,3,4,5};
+		int *ptr1 = (int*)(&a + 1);
+		int *ptr2 = (int*)(a + 1);
+
 		printf("a = %p\n", a);
 		printf("&a = %p\n", &a);
 		printf("a+1 = %p\n", a+1);
 		printf("&a+1 = %p\n", &a+1);
 
+		printf("%x\n", ptr1[-1]);    //the value is 5
+		printf("%x\n", *ptr2);       //the value is 2
 		return 0;
 }
